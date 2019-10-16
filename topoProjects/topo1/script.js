e2e56544de54393c203c60692161cb7ad4f9e2ff
shawn.oakley <shawn.oakley@youvisit.com>
2019-10-16 00:44:04 -0400

    Adds scaling world topo map

--- topoProjects/topo1/script.js
+++ topoProjects/topo1/script.js
@@ -1,22 +1,49 @@
-var width = 960,
-    height = 500;
+// https://medium.com/@aendrew/creating-topojson-using-d3-v4-10838d1a9538
+// https://bl.ocks.org/piwodlaiwo/7d65515de5d1a8bf4b8699d61f5a09b7
+// http://bl.ocks.org/harlantwood/6900108
+// https://bost.ocks.org/mike/map/
+// http://bl.ocks.org/jczaplew/4444770
+// resources
 
-var path = d3.geo.path();
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", window.innerWidth+"px")
+            .attr("height", window.innerHeight+"px")            
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var projection = d3.geoMercator().scale(window.innerWidth/8).translate([window.innerWidth / 2, window.innerHeight / 2]);
+var mapData
+var draw = (mapData) => {
+    var path = d3.geoPath().projection(projection);
+    var url = "https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-50m.json";
+    if (!mapData) {
+        d3.json(url).then( data => {
+            mapData = data
+            svg.selectAll("path")
+            .data(topojson.feature(data, data.objects.countries).features)
+            .enter().append("path")
+            .attr("d", path)
+            .attr("fill", "white")
+            .attr("stroke", "black")
+            .attr("stroke:hover", "red")
+        })
+    } else {
+        svg.selectAll("path")
+        .data(topojson.feature(mapData, mapData.objects.countries).features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("fill", "white")
+        .attr("stroke", "black")
+        .attr("stroke:hover", "red")
+    }   
 
-var url = "https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json"
-d3.json(url, function(error, topology) {
-  if (error) throw error;
-  
-  console.log("topojson", topology)
-  var geojson = topojson.feature(topology, topology.objects.counties);
-  console.log("geojson", geojson)
+}
 
-  svg.selectAll("path")
-      .data(geojson.features)
-    .enter().append("path")
-      .attr("d", path);
-});
+var resize = () => {
+    svg.selectAll("path").remove();
+    projection.scale(window.innerWidth/8).translate([window.innerWidth / 2, window.innerHeight / 2]);
+    draw()
+  }
+
+draw()
+
+window.onresize = resize;
